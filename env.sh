1354d5476cd31894cdee3f1d07312ed200a6cb40
Joe Freeman <joe@freemansoft.com>
2021-08-05 06:49:13 -0400

    use smaller vm during testing

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ region="EastUS"
 
 # add version # to name  to avoid complications with soft-deleted keyvaults while testing
 # keyvault deletes are soft deletes witha purgable recovery == 90 days 2021/07
-root_name="luks-example-8"
+root_name="luks-example-11"
 
 resource_group="rg-$root_name"
 key_vault_name="kv-$root_name"
@@ -19,7 +19,8 @@ identity_name="uai-$root_name"
 # On Linux virtual machines, the temporary disk is typically /dev/sdb and is formatted and mounted to /mnt by the Azure Linux Agent
 # Standard_A2_v2 20gb temp storage
 # Standard_L8s_v2 machines have unmounted NvMe drive per 8 vcore and are expensive
-vm_type="Standard_L8s_v2"
+vm_type="Standard_A2_V2"
+#vm_type="Standard_L8s_v2"
 vm_name="vm-$root_name"
 vm_admin_user="azureuser"
 
