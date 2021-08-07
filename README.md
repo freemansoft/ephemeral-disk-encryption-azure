4e40df8470ba29af59e7c25eced8de4e7cf35fef
Joe Freeman <joe@freemansoft.com>
2021-08-06 21:28:48 -0400

    command-line.sh uses parameterized data.mount and unlock-data.service

--- README.md
+++ README.md
@@ -15,18 +15,39 @@ These scripts allocate LS_v2 machines by default.  They are **expensive** so _te
     * Ubuntu currently has the latest installed. So WSL2 developers running Ubuntu should be fine 
     * `az --version` should be 2.26.1 _or later_
 1. Edit `env.sh` to set the resource names
-1. Run scripts 0,1,2,3 to create resources.  The scripts are re-runable
-    * `0...` install tooling
+1. Run scripts 0,1,2,3 to create resources.  The scripts are re-runable. 
+    * `0...` install tooling.  It may _sudo_ to install snap or Azure CLI
     * `1...` log into Azure using the CLI
     * `2...` create the keyvault and a secret and an identity
     * `3...` create a VM. Associate the identity as user defined. Customize any utility vm files. Copy utility files to the VM via SSH
 1. SSH onto the VM to verify
+    * `ssh azureuser@<ip>` as shown at the end of script `3...sh`
     * Run `get-user-identity-secret.sh` to verify the identity has been applied to the server and the secret is retirevable.
 
 # Luks encrypting the local disk
-1. SSH into the vm
-1. Run `sudo command-line.sh`
-1. run `df` to verify the LUKS mount
+1. SSH into the vm per the output of `3...sh`
+1. `cd vm-tools`
+1. partition the NVMe, encrypt the partion. Add the mount to the /etc
+    * Run `sudo bash command-line.sh`
+1. run `df` and `lsblk` to verify the LUKS mount
+
+# The file system after encryption
+```
+$ lsblk
+NAME        MAJ:MIN RM  SIZE RO TYPE  MOUNTPOINT
+loop0         7:0    0 99.4M  1 loop  /snap/core/11420
+loop1         7:1    0  240K  1 loop  /snap/jq/6
+sda           8:0    0   80G  0 disk
+└─sda1        8:1    0   80G  0 part  /mnt
+sdb           8:16   0   30G  0 disk
+├─sdb1        8:17   0 29.9G  0 part  /
+├─sdb14       8:30   0    4M  0 part
+└─sdb15       8:31   0  106M  0 part  /boot/efi
+sr0          11:0    1  628K  0 rom
+nvme0n1     259:0    0  1.8T  0 disk
+└─nvme0n1p1 259:1    0  1.8T  0 part
+  └─data0   253:0    0  1.8T  0 crypt /data0
+```
 
 # Destroying resources
 * Return to the host
--- vm-files/command-line.sh
+++ vm-files/command-line.sh
@@ -9,45 +9,64 @@ if [[ ${EUID:-$(id -u)} -ne 0 ]]; then
 fi
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-# lunks-key.sh requires jq
+# luks-key.sh requires jq
 if ! command -v jq &> /dev/null
 then
     sudo snap install jq
 fi
 
-cp $DIR/luks-key.sh /etc
-cp $DIR/luks-env.sh /etc
+create_luks_etc_utils(){
+    cp $DIR/luks-key.sh /etc
+    cp $DIR/luks-env.sh /etc
 
-chown root:root /etc/luks-key.sh
-chmod 0500 /etc/luks-key.sh
-chown root:root /etc/luks-env.sh
-chmod 0500 /etc/luks-env.sh
+    chown root:root /etc/luks-key.sh
+    chmod 0500 /etc/luks-key.sh
+    chown root:root /etc/luks-env.sh
+    chmod 0500 /etc/luks-env.sh
+}
 
-# hard coded to the first NVMe drive
-parted /dev/nvme0n1 mklabel gpt
-parted -a opt /dev/nvme0n1 mkpart datadisk xfs 0% 100%
+create_luks_partitions() {
+    parted $DISK_DEVICE mklabel gpt
+    parted -a opt $DISK_DEVICE mkpart datadisk xfs 0% 100%
 
-/etc/luks-key.sh | cryptsetup -d - -v --type luks2 luksFormat /dev/nvme0n1p1
-/etc/luks-key.sh | cryptsetup -d - -v luksOpen /dev/nvme0n1p1 data1
+    /etc/luks-key.sh | cryptsetup -d - -v --type luks2 luksFormat $DISK_PARTITION
+    /etc/luks-key.sh | cryptsetup -d - -v luksOpen $DISK_PARTITION $LUKS_PART_NAME
 
-mkfs.xfs -L data1 /dev/mapper/data1
+    mkfs.xfs -L data1 $LUKS_DEVICE
+    cryptsetup -v luksClose $LUKS_PART_NAME
+}
 
-cryptsetup -v luksClose data1
+create_luks_automounts() {
+    # in unlock-data1.service - replace DRIVE_UUID with $UUID
+    DRIVE_UUID="$(lsblk -o UUID $DISK_PARTITION --noheadings)"
+    #echo $DRIVE_UUID
 
-# in unlock-data1.service - replace DRIVE_UUID with $UUID
-UUID="$(lsblk -o UUID /dev/nvme0n1p1 --noheadings)"
-#echo $UUID
+    cp $DIR/data.mount $MOUNT_DEF
+    chown root:root $MOUNT_DEF
+    sed -i "s:--LUKS_DEVICE--:$LUKS_DEVICE:g" "$MOUNT_DEF"
+    sed -i "s:--LUKS_PART_NAME--:$LUKS_PART_NAME:g" "$MOUNT_DEF"
+    sed -i "s:--SERVICE_DEF_NAME--:$SERVICE_DEF_NAME:g" "$MOUNT_DEF"
 
-# the rest of this script isn't correct until we replace the UUID with the one we looked up
-#exit 0
+    cp $DIR/unlock-data.service $SERVICE_DEF
+    chown root:root $SERVICE_DEF
+    sed -i "s:--DRIVE_UUID--:$DRIVE_UUID:g" "$SERVICE_DEF"
+    sed -i "s:--LUKS_PART_NAME--:$LUKS_PART_NAME:g" "$SERVICE_DEF"
 
-cp $DIR/data1.mount /etc/systemd/system
-chown root:root /etc/systemd/system/data1.mount
+    systemctl daemon-reload
+    mkdir -p /$LUKS_PART_NAME
+    systemctl enable --now $MOUNT_DEF
+}
 
-cp $DIR/unlock-data1.service /etc/systemd/system
-chown root:root /etc/systemd/system/unlock-data1.service
-sed -i 's/DRIVE_UUID/$UUID/' /etc/systemd/system/unlock-data1.service
+# this should loop across all found nvme drives
+DISK_NUM=0
+DISK_DEVICE="/dev/nvme$DISK_NUM""n1"
+DISK_PARTITION="/dev/nvme$DISK_NUM""n1p1"
+LUKS_PART_NAME="data$DISK_NUM"
+LUKS_DEVICE="/dev/mapper/data$DISK_NUM"
+MOUNT_DEF="/etc/systemd/system/data$DISK_NUM.mount"
+SERVICE_DEF="/etc/systemd/system/unlock-data$DISK_NUM.service"
+SERVICE_DEF_NAME="unlock-data$DISK_NUM.service"
 
-systemctl daemon-reload
-mkdir /data1
-systemctl enable --now data1.mount
+create_luks_etc_utils
+create_luks_partitions
+create_luks_automounts
